72e2f7971c47936d027c7de1f7ca5ef145e5a889
chantuff <chan.tuff@yahoo.com>
2023-11-10 15:56:57 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import products from './data'
+
+function App() {
+  console.log(products)
+  // add state in here
+
+  // handleChange function
+
+// handleSubmit
+
+
+
+  // return
+  return (
+
+    <div>
+      <h1> Hi there! </h1>
+    </div>
+  )
+}
+
+export default App;
